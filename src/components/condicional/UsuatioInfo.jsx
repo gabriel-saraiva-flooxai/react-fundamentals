98a430ebbb57d40c2662231a4dbb58ace9acbd6f
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-16 15:37:08 -0300

    feat: conditional render #3

--- src/components/condicional/UsuatioInfo.jsx
+++ src/components/condicional/UsuatioInfo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import If from "./If"
+import If, {Else} from "./If"
 
 export default props => {
     const usuario = props.usuario || {}
@@ -7,9 +7,9 @@ export default props => {
         <div>
             <If test={usuario && usuario.nome}>
                 Parabéns <strong>{ usuario.nome }</strong>!
-            </If>
-            <If test={!usuario || !usuario.nome}>
-                Parabéns <strong>Parça</strong>!
+                <Else>
+                    Parabéns <strong>Parça</strong>!
+                </Else>
             </If>
         </div>
     )
