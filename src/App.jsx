f6e7cc92f400832d2b1cc5a6b05dcec8219449a3
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-16 01:58:25 -0300

    feat: finish cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import React from "react";
 
 import Card from "./components/layout/Card";
@@ -8,18 +9,19 @@ import First from "./components/basics/First";
 
 export default _ =>
 
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React</h1>
 
-        <Card titulo="#04 - Ramdom">
+        <div className='Cards'>
+        <Card titulo="#04 - Ramdom" color="#080">
             <Random min={1} max={60} />
         </Card>
 
-        <Card titulo="#03 - Fragment">
+        <Card titulo="#03 - Fragment" color="#080">
         <Fragment />
         </Card>
 
-        <Card titulo="#02 - With Parameter">
+        <Card titulo="#02 - With Parameter" color="#080">
         <WithParameter
             titulo='Situação do Aluno'
             aluno='Saraiva'
@@ -27,8 +29,9 @@ export default _ =>
         />
         </Card>
 
-        <Card titulo="#01 - First">
+        <Card titulo="#01 - First" color="#080">
             <First></First>
         </Card>
+        </div>
 
     </div>
