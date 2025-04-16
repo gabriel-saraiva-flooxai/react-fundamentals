c8aef66f182193425b8ca6a44e630f37821bcc93
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-15 23:46:37 -0300

    feat: implement cards to split the exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Card from "./components/layout/Card";
 import Random from "./components/basics/Random";
 import Fragment from "./components/basics/Fragment";
 import WithParameter from "./components/basics/WithParameter";
@@ -9,12 +10,25 @@ export default _ =>
 
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Random min={1} max={60} />
+
+        <Card titulo="#04 - Ramdom">
+            <Random min={1} max={60} />
+        </Card>
+
+        <Card titulo="#03 - Fragment">
         <Fragment />
+        </Card>
+
+        <Card titulo="#02 - With Parameter">
         <WithParameter
             titulo='Situação do Aluno'
             aluno='Saraiva'
             nota={6.5}
         />
-        <First></First>
+        </Card>
+
+        <Card titulo="#01 - First">
+            <First></First>
+        </Card>
+
     </div>
